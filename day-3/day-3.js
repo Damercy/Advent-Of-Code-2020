091631c83d4fe6da7af0e91f61ae6d412a5123e2
Damercy <adhikaridayamoy@gmail.com>
2020-12-07 22:18:41 +0530

    Part-1 done

--- day-3/day-3.js
+++ day-3/day-3.js
@@ -2,5 +2,21 @@ const fs = require("fs");
 
 const input = fs.readFileSync("./input", { encoding: "utf-8" }).split("\r\n");
 
-// console.log(input[0])
-// console.log(input[0][31])
+
+const noOfCols = input[0].length;
+const noOfRows = input.length;
+let trees = 0, slopeLevel=0, colLevel=0;
+
+
+if(input[slopeLevel][colLevel]==="#")
+    trees++;
+
+while(slopeLevel<noOfRows-1){
+    slopeLevel+=1;
+    colLevel=(colLevel+3)%noOfCols;
+    console.log(`slopeLevel:${slopeLevel} ::: colLevel:${colLevel}`);
+    if(input[slopeLevel][colLevel]==="#")
+        trees++;
+}
+
+console.log(`No. of trees:${trees}`);
