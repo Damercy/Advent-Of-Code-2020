2c52ccfc12ca0d85d51c83a0bda88cf4735d9eff
Damercy <adhikaridayamoy@gmail.com>
2020-12-08 22:40:57 +0530

    Parsing is tough; Checkout tomorrow;

--- day-4/day-4.js
+++ day-4/day-4.js
@@ -1,7 +1,27 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("./input",{encoding:"utf-8"}).split("\r\n")
+const input = fs.readFileSync("./input",{encoding:"utf-8"}).split("\r\n");
+input.push("");
 
 const fieldsRequired = ["byr","iyr","eyr","hgt","hcl","ecl","pid","cid"];
 
-console.log(input);
+function getNoOfValidPassports(){
+    const formattedInput = input.map((entry,idx) =>{
+        //console.log(entry)
+        const finalEntry=[]
+        const entryValue = entry.split(" ").map(p =>p.split(":"));
+        //finalEntry = entryValue.
+
+        //console.log(entryValue);
+        entryValue.forEach(val => {
+            if(val[0].length===0)
+
+            else{
+
+            }
+
+        });
+    });
+}
+
+getNoOfValidPassports();
